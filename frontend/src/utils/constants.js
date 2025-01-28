d5ebfd900a9cb7697a9ba53cc39e3ad3db8da475
Salem-Mo <124922909+Salem-Mo@users.noreply.github.com>
2025-01-28 23:13:55 +0200

    n

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const ClientURL='https://resku-production.up.railway.app'
 // export const ServerUrl='http://192.168.1.2:5000'
-export const ServerUrl='resku-backend-production.up.railway.app'
+export const ServerUrl='https://resku-backend-production.up.railway.app'
 
