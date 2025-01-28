991925fe7a73c597f6645760084409fdb5ac3caf
Salem-Mo <124922909+Salem-Mo@users.noreply.github.com>
2025-01-28 23:56:54 +0200

    n

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 const FEHosturl = process.env.FEHosturl;
 
-app.use(cors({ origin: FEHosturl, credentials: true }));
+// app.use(cors({ origin: FEHosturl, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
 export const ClientURL='https://resku-production.up.railway.app'
-// export const ServerUrl='http://192.168.1.2:5000'
+// export const ClientURL='http://localhost:5173'
 export const ServerUrl='https://resku-backend-production.up.railway.app'
+// export const ServerUrl='http://localhost:5000'
 
