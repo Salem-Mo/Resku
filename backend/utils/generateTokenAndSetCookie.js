4521ca7bc66b3ad642afd73d6aa0bb72d2bd44d0
Salem-Mo <124922909+Salem-Mo@users.noreply.github.com>
2025-02-22 17:31:12 +0200

    s

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -1,25 +1,22 @@
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
-// dotenv.config();
-// export const generateTokenAndSetCookie = (res, userId) => {
-// 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-// 		expiresIn: "7d",
-// 	});
-
-// 	res.cookie("token", token, {
-// 		httpOnly: true,
-// 		secure: process.env.NODE_ENV === "production",
-// 		sameSite: "strict",
-// 		maxAge: 7 * 24 * 60 * 60 * 1000,
-// 	});
-
-// 	return token;
-// };
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+export const generateTokenAndSetCookie = (res, userId) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "7d",
+	});
 
-export const generateTokenAndSetCookie = (res, userId, isMobile = false) => {
+	res.cookie("token", token, {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict",
+		maxAge: 7 * 24 * 60 * 60 * 1000,
+	});
+
+	return token;
+};
+
+export const generateTokenAndSetHeader = (res, userId, isMobile = false) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
